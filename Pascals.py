a5859bb133b9f1e96b54fa3b1d2f895b8c89bf1f
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:48:49 +0530

    Update Pascals.py

--- Pascals.py
+++ Pascals.py
@@ -1,9 +1,16 @@
 #pascals triangle 
-n=int(input("enter no. of rows "))
-# iterarte upto n
-for i in range(n):
-    # adjust space
-    print(' '*(n-i), end='')
+ n=int(input("enter no. of rows "))
+ def func(n,b):
+   r = 1
+   if (b> n - b):
+       b = n- b
+   for i in range(0,b):
+       r = r * (n-i)
+       r = r //(i+1)
+   return r
+   
+ for i in range (0, n):
+     for j in range(0, i + 1):
+       print(func(i,j)," ", end="")
+     print()
  
-    # compute power of 11
-    print(' '.join(map(str, str(11**i))))

c125aa1f01fd7fa1bcd4bd02ae2ae62dec561ad2
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:19:54 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+#create a tuple. 
+t=(2,4,6,9)
+#create a tuple with different data types. 
+t1=(2,3,4,'reena','s','&')
+print(t1)
+#create a tuple with numbers and print one item. 
+print(t1[-1])
+#add two tuples. 
+t2=t1+t
+print(t2)
+#convert a tuple to a string. 
+t=('M','I','T','A','S','H','I')
+str= ''.join(t)
+print(str)

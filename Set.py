0efb6da379452e36fcea32644b77bf708ed4954b
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:21:18 +0530

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,12 @@
+#create a set.
+s1={1,2,3,4}
+print(s1)
+#add member(s) in a set.
+s1.add('mita')
+print(s1)
+#remove item(s) from set
+s1.discard('mita')
+print(s1)
+#remove an item from a set if it is present in the set.
+s1.remove(4)
+print(s1)

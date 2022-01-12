8500d20b310b60cd2c0aa1d65509d693eee8ff83
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:40:58 +0530

    Create Left_star_pattern.py

---
+++ Left_star_pattern.py
@@ -0,0 +1,19 @@
+#left star pattern 
+def pattern(n):
+  k=n
+  for i in range(0,n):
+    for j in range(0,k):
+      print(end="  ")
+    k=k-1
+    for j in range(0,i+1):
+      print("*",end=" ")
+    print("\r")
+  k =-1
+  for i in range(n,-1,-1):
+    for j in range(k,-1,-1):
+      print(end=" ")
+    k=k+2
+    for j in range(0,i+1):
+      print("*",end=" ")
+    print("\r")
+pattern(3)

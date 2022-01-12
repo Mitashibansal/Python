18fd16d99c2ddefa6f818a54e21435bcd40e8fb5
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:33:24 +0530

    Create pythagorean_no.py

---
+++ pythagorean_no.py
@@ -0,0 +1,8 @@
+from math import sqrt
+n =int(input('Maximum number (no. must be greater than 4) '))
+for a in range (1,n+1):
+  for b in range (a,n):
+    c_square=a**2 + b**2
+    c= int(sqrt(c_square))
+    if ((c_square - c**2)==0) & (c<=n):
+      print(a,b,c)

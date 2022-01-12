87862638c0e7763c6921d32513ee84c0097e4d78
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 17:29:55 +0530

    Create Guess_Game.py

---
+++ Guess_Game.py
@@ -0,0 +1,16 @@
+import random
+n=20
+to_be_guessed = int (n* random.random())+1
+guess=0
+while guess!= to_be_guessed:
+    guess=int(input("new number :"))
+    if guess > 0:
+       if guess > to_be_guessed:
+          print("no is too large ")
+       elif  guess< to_be_guessed:
+          print("no is too small ")
+    else :
+       print("are u giving up :( ")
+       break
+else:
+   print("congratulations u won :) ")

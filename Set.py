d51145be832d95d1aa13f09ae88167841f5fdd5b
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 18:16:20 +0530

    Update Set.py

--- Set.py
+++ Set.py
@@ -10,3 +10,18 @@ print(s1)
 #remove an item from a set if it is present in the set.
 s1.remove(4)
 print(s1)
+for i in range(int(input())):
+    a = int(input());
+    A = set(input().split()) 
+    b = int(input()); 
+    B = set(input().split())
+    print(A.issubset(B))
+    
+s = set(input().split())
+ans = True
+for i in range(int(input())):
+    t = set(input().split())
+    if (s > t) == False:
+        ans = False
+        break
+print(ans)

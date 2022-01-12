18a5040c502057f45929d6e2a8fdcc165e4dc8e1
Mitashibansal <84284893+Mitashibansal@users.noreply.github.com>
2022-01-12 18:14:51 +0530

    Update Fact.py

--- Fact.py
+++ Fact.py
@@ -13,3 +13,16 @@ n=int(input(" "))
 def factorial(n):
   return 1 if (n==1 or n==0) else n * factorial(n - 1);
 print(factorial(n),end="")
+# Factorial series
+n= int(input("enter a number till you want a series :"))
+def Factorial(n):
+ if n < 0:
+   print("Incorrect input enter a positive number")
+ elif n == 0 or n == 1:
+   return 1
+ else:
+   for j in range(n+1):
+     return n*Factorial(n-1)
+for i in range(n+1):
+  print(Factorial(i),end=" ") 
+print( "\nanswer term is:",Factorial(n))
